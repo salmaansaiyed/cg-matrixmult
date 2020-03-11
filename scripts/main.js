5a74f1ea238a7a5321da6631cca8f8a2526ade26
salmaansaiyed <salmaan.saiyed@gmail.com>
2020-03-11 12:11:46 -0500

    working on ChangeTransform function

--- scripts/main.js
+++ scripts/main.js
@@ -10,7 +10,14 @@ function CalculateCompoundTransform(transforms) {
     // `compound_transform = Matrix.multiply(...)`
     var tranform_matrices = [];
 
-    compound_transform = new Matrix(4, 4); // change / remove this
+    if (transforms.length == 1) {
+        compound_transform = transforms[0].mat4x4;
+    } else {
+        for (var i = 1; i < transforms.length; i++) {
+            compound_transform = transforms[i];
+            compound_transform = Matrix.multiply(compound_transform.mat4x4,transforms[i].mat4x4);
+        }
+    }
 
     return compound_transform;
 }
@@ -28,7 +35,18 @@ function CalculateTransformedVertex(vertex) {
 function ChangeTransform(index, type, values) {
     app.transforms[index].type = type;
     // update `app.transforms[index].mat4x4`
-
+    // transform options: translate, scale, rotate_x, rotate_y, rotate_z
+    if (type.equals("translate")) {
+        app.transforms[index].mat4x4 = Mat4x4Translate(mat4x4, tx, ty, tz);
+    } else if (type.equals("scale")) {
+        app.transforms[index].mat4x4 = Mat4x4Scale(mat4x4, sx, sy, sz);
+    } else if (type.equals("rotate_x")) {
+        app.transforms[index].mat4x4 = Mat4x4RotateX(mat4x4, theta);
+    } else if (type.equals("rotate_y")) {
+        app.transforms[index].mat4x4 = Mat4x4RotateY(mat4x4, theta);
+    } else if (type.equals("rotate_z")) {
+        app.transforms[index].mat4x4 = Mat4x4RotateZ(mat4x4, theta);
+    }
     // recalculate compound transform and tranformed vertex
     app.compound = CalculateCompoundTransform(app.transforms);
     app.final_vertex = CalculateTransformedVertex(app.vertex);
